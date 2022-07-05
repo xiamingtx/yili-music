0638147ab906c22c9427426e82ea2764eb2fc706
xiamingtx <1730821268@qq.com>
2022-07-05 21:39:23 +0800

    add CrossOrigin

--- src/main/java/com/bilitech/yilimusic/controller/DefaultController.java
+++ src/main/java/com/bilitech/yilimusic/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package com.bilitech.yilimusic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
 
     @GetMapping
